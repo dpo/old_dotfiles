405841a38f9f73a08a75229ef415faedf7951ad7
Dominique Orban <dominique.orban@gmail.com>
2014-12-27 17:39:26 -0500

    Link vim files.

--- init_vim.sh
+++ init_vim.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+dotfiles=$PWD
+
 # Install Vim-Pathogen.
 [[ ! -d ~/.vim ]] && mkdir ~/.vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim
+ln -s ${dotfiles}/.vim/*.sh .
+
+# Link other vim-related dotfiles.
+cd ~
+ln -s ${dotfiles}/.vimrc .
+ln -s ${dotfiles}/.vim.local .
+ln -s ${dotfiles}/.vimpagerrc .
+
